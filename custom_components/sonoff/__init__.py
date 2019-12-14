4a166008c54b04ad1924c15488f0d908279933b7
Will Boyce <me@willboyce.com>
2019-12-14 21:27:13 +0000

    wip

--- custom_components/sonoff/__init__.py
+++ custom_components/sonoff/__init__.py
@@ -682,6 +682,11 @@ class SonoffDevice(Entity):
         if 'rssi' in device['params']:
             self._attributes['rssi'] = device['params']['rssi']
 
+        # Diffuser (TODO: map RGB into lightcolor, lightbright<->brightness, state<->mode, sledOnline<->status_led)
+        for key in ('water', 'lightmode', 'lightbright', 'lightRcolor', 'lightGcolor', 'lightBcolor', 'state', 'sledOnline'):
+            if key in device['params']:
+                self._attributes[key] = device['params'][key]
+
         # the device has more switches
         if self._outlet is not None:
             return device['params']['switches'][self._outlet]['switch'] == 'on' if device else False
@@ -722,3 +727,11 @@ class SonoffDevice(Entity):
     def device_state_attributes(self):
         """Return device specific state attributes."""
         return self._attributes
+
+    @property
+    def device_info(self):
+        return {
+            'identifiers': ('sonoff', self._deviceid),
+            'name': self._name,
+            'manufacturer': 'Sonoff',
+        }
